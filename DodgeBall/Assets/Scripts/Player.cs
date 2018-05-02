3e21cf88ec39afeac4101bc16dcba80504e60bfe
William Ngo <ngo.willi@gmail.com>
2018-05-02 14:53:35 -0400

    added player scripts
    still tweaking it

---
+++ DodgeBall/Assets/Scripts/Player.cs
@@ -0,0 +1,231 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(RaycastController))]
+public class Player : MonoBehaviour
+{
+
+	/* STATS */
+	public int health;
+	public bool invincible;
+	public bool hurt;
+	public float invulnerableTime = 2f;
+
+	/* MOVEMENT */
+	public float gravity; //Amount of velocity y to fall
+	public float jumpHeight; //Height of jump
+	public float minJumpHeight;
+	public float speed; //Horizontal movement speed
+
+	public int facing = 1;
+	//1 means facing right, -1 means left
+	public bool isAirborne;
+	public bool isDoubleJumping;
+
+	public Vector2 velocity;
+	public Vector2 directionalInput;
+
+	private RaycastController rc;
+	private SpriteRenderer render;
+	private Animator animator;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rc = GetComponent<RaycastController> ();
+		render = GetComponent<SpriteRenderer> ();
+		animator = GetComponent<Animator> ();
+
+		health = 3;
+
+		//Kinematic formula, solve for acceleration going down
+		gravity = -(2 * 2.5f) / Mathf.Pow (0.36f, 2);
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//Hurt boolean gives this 'pause' so that player doesn't keep moving
+		//during the reeling animation of being hit by a trap.
+		if (!hurt) {
+			GetPlayerInput ();
+			velocity.x = directionalInput.x * speed;
+			velocity.y += gravity * Time.deltaTime; //Gravity makes game obj fall at all times
+			//Move game object'
+			Move (velocity * Time.deltaTime);
+		}
+		//Checks current state of game obj and makes adjustment to velocity if necessary
+		checkState ();
+	}
+
+	/// <summary>
+	/// This trigger will check for collision with traps. Not the level.
+	/// If collided with traps, player's health reduces and becomes invulnerable
+	/// for a short while.
+	/// if item, then pick up
+	/// </summary>
+	/// <param name="other">Other.</param>
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		/*
+		if (!invincible) {
+			if (other.tag == "Trap") {
+				ReceiveDamage ();
+			}
+		}*/
+
+		if (other.tag == "Coin") {
+			coins++;
+		}
+	}
+
+	public void ReceiveDamage()
+	{
+		if (!invincible) {
+			velocity.y = 0;
+			animator.Play ("player_hurt");
+			//Receive damage
+			health--;
+			Debug.Log ("HEALTH: " + health);
+
+			//Makes slight pause and prevent player from moving when hit
+			hurt = true;
+			Invoke ("resetHurt", 0.2f);
+
+			//Become invulnerable for 2 seconds
+			invincible = true;
+			Invoke ("resetInvincible", invulnerableTime);
+		}
+	}
+
+	/// <summary>
+	/// Moves the player. Raycast only checks for level collision such as walls and floors, not traps.
+	/// </summary>
+	/// <param name="moveAmount">Move amount.</param>
+	public void Move (Vector2 moveAmount)
+	{
+		//Updates raycast position as game object moves.
+		rc.UpdateRayOrigins ();
+		rc.collision.Reset ();
+
+		//direction player is facing; right = 1, left = -1
+		setFacingDirection ();
+
+		//Check collisions - if found, moveAmount velocity will be reduced appropriately
+		rc.checkCollisions (ref moveAmount);
+
+		//Actually changing the velocity of game object
+		transform.Translate (moveAmount);
+	}
+
+	/// <summary>
+	/// This method checks the state of the game object after moving and makes any approriate changes 
+	/// should it encounter states such as being airborne, grounded or interacting with incoming obstacle.
+	/// Called after moving
+	/// </summary>
+	private void checkState ()
+	{
+		//If grounded, reset airborne and double jump state
+		if (rc.collision.below) {
+			velocity.y = 0f;
+			isAirborne = false;
+			isDoubleJumping = false;
+		} else if (!rc.collision.below) { //when airborne
+			isAirborne = true;
+		}
+		animator.SetBool ("airborne", isAirborne); //Set sprite to airborne animation
+		//Falling animation HERE
+
+		//If hit ceiling, set velocity.y to 0 to prevent accumulating
+		if (rc.collision.above) { 
+			velocity.y = 0f;
+			Debug.Log ("Hit ceiling");
+		}
+
+		//Apparantly, Color isn't something you can modify like transform.position
+		//Reduce transparency by half when hurt.
+		Color c = render.color;
+		if (invincible) {
+			c.a = 0.5f;
+		} else {
+			c.a = 1f;
+		}
+		render.color = c;
+	}
+
+	/// <summary>
+	/// Sets the facing direction of game object
+	/// </summary>
+	private void setFacingDirection ()
+	{
+		//Keeps game object facing direction when not moving
+		if (velocity.x != 0) {
+			rc.collision.collDirection = (int)Mathf.Sign (velocity.x);
+		}
+
+		//Flips sprite
+		bool dir = render.flipX ? (directionalInput.x > 0) : (directionalInput.x < 0);
+		if (dir) {
+			render.flipX = !render.flipX;
+		}
+	}
+
+	/// <summary>
+	/// Resets the invincble boolean. Used by OnTriggerEnter2D, to return player to vulnerable state 
+	/// after slight moment of invincibility.
+	/// </summary>
+	private void resetInvincible ()
+	{
+		invincible = false;
+	}
+
+	/// <summary>
+	/// reset hurt boolean. Used in update(), to allow player to move again.
+	/// </summary>
+	private void resetHurt ()
+	{
+		hurt = false;
+	}
+
+	/**
+	 * USER INPUT
+	 * 
+	 */
+	private void GetPlayerInput ()
+	{
+		this.directionalInput = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		animator.SetFloat ("velocityX", Mathf.Abs (directionalInput.x));
+
+		//Jump
+		if (Input.GetKeyDown ("j")) {
+			OnJumpDown ();
+		}
+
+		if (Input.GetKeyUp ("j")) {
+			OnJumpUp ();
+		}
+	}
+
+	private void OnJumpUp()
+	{
+		if (velocity.y > 8f) {
+			velocity.y = 8f;
+		}
+	}
+
+	private void OnJumpDown ()
+	{
+		if (rc.collision.below) {
+			isAirborne = true;
+			velocity.y = jumpHeight;
+		}
+
+		//Double jump
+		if (!isDoubleJumping && !rc.collision.below) {
+			velocity.y = jumpHeight * .8f;
+			isDoubleJumping = true;
+		}
+	}
+}
---
+++ DodgeBall/Assets/Scripts/RaycastController.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(BoxCollider2D))]
+public class RaycastController : MonoBehaviour
+{
+	//Layer in which to detect a collision
+	public LayerMask collisionMask;
+
+	public const float widthBuffer = .015f;
+	//Buffer distance
+	private const float dstBetweenRays = .25f;
+	//Should have 4 rays for each side
+
+	public int rayCount_h;
+	//number of horizontal rays
+	public int rayCount_v;
+	//number of vertical rays
+
+	public float raySpacing_h;
+	public float raySpacing_v;
+
+	private BoxCollider2D box;
+	//box collider of game object
+
+	//Raycast origins
+	public Vector2 topLeft;
+	public Vector2 topRight;
+	public Vector2 botLeft;
+	public Vector2 botRight;
+
+	//Collisions information
+	public CollisionInformation collision;
+
+	public virtual void Awake ()
+	{
+//		player = GetComponent<Player> ();
+		box = GetComponent<BoxCollider2D> ();
+	}
+
+	void Start ()
+	{
+		/*** Set up raycasts ***/
+		//Get bounds of boxcollider with a bit of buffer
+		Bounds bounds = box.bounds;
+		bounds.Expand (widthBuffer * -2);
+
+		float width = bounds.size.x;
+		float height = bounds.size.y;
+
+		//Set direction, by default we face right;
+		collision.collDirection = 1;
+
+		//Calculate number of rays
+		rayCount_h = Mathf.RoundToInt (height / dstBetweenRays);
+		rayCount_v = Mathf.RoundToInt (width / dstBetweenRays);
+
+		//Space them out evenly
+		raySpacing_h = height / (rayCount_h - 1);
+		raySpacing_v = width / (rayCount_v - 1);
+	}
+
+	/**
+	 * Update point of origin of raycast when boxcollider moves.
+	 *
+	 */
+	public void UpdateRayOrigins ()
+	{
+		//Get bounds of boxcollider with a bit of buffer
+		Bounds bounds = box.bounds;
+		bounds.Expand (widthBuffer * -2);
+
+		//set raycast origin point
+		topLeft = new Vector2 (bounds.min.x, bounds.max.y);
+		topRight = new Vector2 (bounds.max.x, bounds.max.y);
+		botLeft = new Vector2 (bounds.min.x, bounds.min.y);
+		botRight = new Vector2 (bounds.max.x, bounds.min.y);
+	}
+
+	/// <summary>
+	/// Checks for both horizontal and vertical collisions. 
+	/// If collisions are encountered, moveAmount will be adjusted
+	/// </summary>
+	/// <param name="moveAmount">Move amount.</param>
+	public void checkCollisions (ref Vector2 moveAmount)
+	{
+		checkHorizontalCollision (ref moveAmount);
+
+		if (moveAmount.y != 0) {
+			checkVerticalCollisions (ref moveAmount);
+		}
+	}
+
+	/// <summary>
+	/// Checks for horizontal collisions ON WALLS.
+	/// </summary>
+	/// <param name="moveAmount">Move amount.</param>
+	private void checkHorizontalCollision (ref Vector2 moveAmount)
+	{
+		float directionX = collision.collDirection;
+		float rayLength = Mathf.Abs (moveAmount.x) + widthBuffer;
+		Vector2 rayDirection = Vector2.right * directionX;
+
+		//Creates raycast
+		for (int i = 0; i < rayCount_h; i++) {
+			//If obj facing right, first raycast starts from bottomRight, else start from bottomLeft
+			Vector2 rayOrigin = directionX == 1 ? botRight : botLeft;
+
+			//Each iteration will create a raycast and each raycast will be spaced according to raySpacing_h
+			rayOrigin += Vector2.up * raySpacing_h * i;
+
+			//Create raycast
+			RaycastHit2D hit = Physics2D.Raycast (rayOrigin, rayDirection, rayLength, collisionMask);
+
+			//Shows raycast in scene viewer
+			Debug.DrawRay (rayOrigin, rayDirection, Color.red);
+
+			if (hit) {
+				//If already colliding with obstacle, leave as is.
+				if (hit.distance == 0) {
+					continue;
+				}
+				
+				//Reduce velocity.x, prevent from going through obstacle
+				moveAmount.x = (hit.distance - widthBuffer) * directionX;
+				rayLength = hit.distance;
+				
+				//Set which side has collided
+				collision.right = (directionX == 1);
+				collision.left = (directionX == -1);
+
+			}
+		}
+	}
+
+
+	/// <summary>
+	/// Checks vertical collision for FLOORS and CEILINGS
+	/// </summary>
+	/// <param name="moveAmount">Move amount.</param>
+	private void checkVerticalCollisions (ref Vector2 moveAmount)
+	{
+		float directionY = Mathf.Sign (moveAmount.y);
+		float rayLength = Mathf.Abs (moveAmount.y) + widthBuffer;
+		Vector2 rayDirection = Vector2.up * directionY;
+
+//		//firing raycast
+		for (int i = 0; i < rayCount_v; i++) {
+			Vector2 rayOrigin = (directionY == -1) ? botLeft : topLeft;
+			rayOrigin += Vector2.right * (raySpacing_v * i + moveAmount.x);
+			RaycastHit2D hit = Physics2D.Raycast (rayOrigin, rayDirection, rayLength, collisionMask);
+
+			Debug.DrawRay (rayOrigin, rayDirection, Color.red);
+
+			//If collision found, update moveAmount.y by reducing the change in velocity
+			if (hit) {
+				moveAmount.y = (hit.distance - widthBuffer) * directionY;
+				rayLength = hit.distance;
+
+				//Set collision bool
+				collision.below = (directionY == -1);
+				collision.above = (directionY == 1);
+			}
+		}
+	}
+
+	public struct CollisionInformation
+	{
+		public bool above, below;
+		public bool left, right;
+
+		public int collDirection;
+		public bool standingOnPlatform;
+		//Not used
+
+		public void Reset ()
+		{
+			above = below = false;
+			left = right = false;
+		}
+	}
+}
