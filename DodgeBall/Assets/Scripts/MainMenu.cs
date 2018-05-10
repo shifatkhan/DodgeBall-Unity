60b915fbdf585c0df6d4854e08436ed01184cb24
William Ngo <ngo.willi@gmail.com>
2018-05-10 00:29:13 -0400

    Pause menu and game over when one player loses all lives

--- DodgeBall/Assets/Scripts/MainMenu.cs
+++ DodgeBall/Assets/Scripts/MainMenu.cs
@@ -7,6 +7,6 @@ public class MainMenu : MonoBehaviour {
 
 	public void PlayGame()
     {
-        SceneManager.LoadScene("WillyScene");
+        SceneManager.LoadScene("WilliamScene");
     }
 }
---
+++ DodgeBall/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour {
+
+    public bool gameIsPaused;
+
+    public GameObject pauseUI;
+
+    void Start()
+    {
+        //Make sure game is NOT paused when you start game
+        //Fixes issue when going from game to mmenu and back to game which keeps
+        //GameIsPaused to true
+        gameIsPaused = false;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    public void PauseGame()
+    {
+        pauseUI.SetActive(true);
+
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseUI.SetActive(false);
+
+        //Restore normal game speed.
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
